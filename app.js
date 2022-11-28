f947084f1886018b91569720663bc3aae8bf6014
gincegeo@gmail.com <gincegeo@gmail.com>
2022-11-28 10:01:53 +0530

    404 page, search style isssue fixed

--- app.js
+++ app.js
@@ -64,18 +64,18 @@ app.use('/admin', adminRouter);
 /* -------------------------------------------------------------------------- */
 /*                catch 404 and forward to error handler                      */
 /* -------------------------------------------------------------------------- */
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 /* -------------------------------------------------------------------------- */
 /*                      error handler middleware                              */
 /* -------------------------------------------------------------------------- */
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('user/404');
+});
 
 module.exports = app;
